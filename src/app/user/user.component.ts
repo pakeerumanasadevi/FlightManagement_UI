9fe6a6d79cc4fc7b3757fc5e236f7c8d05751bf8
pakeerumanasadevi <pakkiru.manasa98@gmail.com>
2020-11-27 20:59:26 +0530

    final commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FlightService } from '../flight.service';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +11,9 @@ import { FlightService } from '../flight.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  faPlusCircle = faPlusCircle;
+  faEye = faEye;
+  faAlignJustify=faAlignJustify;
 
   constructor(private router: Router, private flightService: FlightService) { }
 
